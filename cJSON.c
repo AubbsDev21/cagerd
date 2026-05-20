5ecd5a8c3a839e5339e426a8c0b3bd4f6ed1e8cc
AubbsDev21 <aubbsdev21@gmail.com>
2026-05-20 14:07:38 -0400

    Writing main.c for cli args and error/loggins as well feat: creating the cli args feat: Parsing args feat: adding error handling

---
+++ cJSON.c
@@ -0,0 +1,5 @@
+#include "cJSON.h"
+
+/* TODO: implement cJSON parse/print/query/free functions */
+/* Drop in the upstream cJSON.c here when ready:           */
+/*   https://github.com/DaveGamble/cJSON/blob/master/cJSON.c */
